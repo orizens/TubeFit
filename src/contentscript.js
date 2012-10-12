bfc1b485054ad97fcf788a82928594b8ba15ef95
Oren Farhi <farhioren@gmail.com>
2012-10-12 09:59:17 +0200

    changed structure

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Oren Farchi / Orizen Designs. All rights reserved.  Use of this
+ * Copyright (c) 2012 Oren Farhi. All rights reserved.  Use of this
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
